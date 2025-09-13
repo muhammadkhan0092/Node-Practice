8b8aa10953970d3025ed6c7b67099a8cb791707d
muhammadkhan0092 <muhammadkhancs0092@gmail.com>
2025-09-13 22:51:51 +0500

    Project Restructuring

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const app = express();
-const userRoutes = require("./routes/userRoutes");
+const mongoose = require("mongoose");
+const userOfflineRoutes = require("./routes/userRoutesOffline");
+const userOnlineRoutes = require("./routes/userRoutesOnline");
 const dbConfig = require("./config/db");
 
 
 
 const reqLoggingMiddleware = require("./middlewares/loggingMiddleware");
 const errorloggingMiddleware = require("./middlewares/errorMiddleware");
+
+
+mongoose.connect('mongodb://localhost:27017/user-app')
+.then(()=>console.log("Mongo Connected"))
+.catch((err)=>console.log(`Mongo Connection Failed ${err}`));
+
+//SCHEMA
+const userSchema = mongoose.Schema({
+  user_id:{
+    type : String,
+    unique:true,
+    required:true
+  },
+  user_name:{
+    type : String,
+    required : true
+  }
+},{timestamps:true});
+
+const User = mongoose.model("user",userSchema);
+User.create({
+  user_id : 1,
+  user_name:"Muhammad Khan",
+});
 
 
 
@@ -26,7 +52,7 @@ dbConfig
   });
 
 //ROUTES
-app.use("/users", userRoutes);
+app.use("/users", userOfflineRoutes);
 
 //SERVER SETUP
 app.listen(3002, (err) => {

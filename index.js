bf5c1dcb33424ce1e28ba7ca920295d4a3f436c3
muhammadkhan0092 <muhammadkhancs0092@gmail.com>
2025-10-11 17:36:42 +0500

    One to One Chat Using Web Sockets

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const userOfflineRoutes = require("./routes/userRoutesOffline");
 const userOnlineRoutes = require("./routes/userRoutesOnline");
 const adminRoutes = require("./routes/adminRoutes");
 const dbConfig = require("./config/db");
-
+const http = require("http");
+const {Server} = require("socket.io");
 
 
 const reqLoggingMiddleware = require("./middlewares/loggingMiddleware");
@@ -41,10 +42,58 @@ app.use("/admin",adminRoutes);
 //app.use("/users", userOfflineRoutes);
 
 //SERVER SETUP
-app.listen(3002, (err) => {
-  if (err) {
-    console.log("Error Starting Server");
-  } else {
-    console.log("Server Started");
+const users = {}
+const server = http.createServer(app);
+const io = new Server(server);
+io.on("connection",(socket)=>{
+  console.log("USER CONNECTED ",socket.id);
+
+  socket.on("register_chat",(userId)=>{
+    console.log("USER WITH USER ID -> ",userId," AND SOCKET ID ",socket.id," REGISTER FOR CHAT");
+    users[userId] = socket.id;
+  })
+
+
+
+
+
+socket.on("private_message", (payload) => {
+  console.log("RAW DATA RECEIVED:", payload);
+
+  // If payload is a string, parse it to JSON
+  let data;
+  try {
+    data = typeof payload === "string" ? JSON.parse(payload) : payload;
+  } catch (e) {
+    console.error("Invalid JSON payload:", e);
+    return;
+  }
+
+  const { senderId, receiverId, message } = data;
+  console.log("MESSAGE RECEIVED:", message);
+  console.log("USERS ARE ",users);
+  const receiversSocketId = users[receiverId];
+  if (receiversSocketId) {
+    console.log("Sending message to:", receiversSocketId);
+    socket.to(receiversSocketId).emit("receive_private", { senderId, message });
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+  socket.on("disconnect",()=>{
+    console.log("USER DISCONNECTED : ",socket.id);
+  });
+});
+
+server.listen(3002,()=>{
+  console.log("Server Started At Port 3002");
+})
